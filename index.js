b1f5d063be87c8296b758d107c6e31a3821f0b7d
shgtkshruch <shgtk.shruch@gmail.com>
2014-10-31 07:18:08 +0900

    Add directory to organize downloaded images.

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
+var fs = require('fs');
+var rimraf = require('rimraf');
+var async = require('async');
 var Slide = require('./lib/slide');
 var Imagemagic = require('./lib/imagemagic');
 
-var slide = new Slide();
-var imagemagic = new Imagemagic();
+var dir = 'images/';
 
-slide.download(function () {
-  imagemagic.convert();
+async.series([
+  function (cb) {
+    fs.mkdir(dir, function () {
+     cb(null);
+    });
+  },
+  function (cb) {
+    var slide = new Slide(dir);
+    slide.download(function () {
+     cb(null);
+    });
+  },
+  function (cb) {
+    var imagemagic = new Imagemagic(dir);
+    imagemagic.convert(function () {
+     cb(null);
+    });
+  },
+  function (cb) {
+    rimraf(dir, function (err) {
+      if (err) {
+        throw err;
+      }
+      console.log('remove ' + dir + 'directory.');
+      cb(null);
+    });
+  }
+], function (err, result) {
+  if (err) {
+    throw err;
+  }
+  console.log('done');
 });
