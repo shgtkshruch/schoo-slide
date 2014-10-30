b380d36a17b1ed41cc08d83c7e701f432a972a08
shgtkshruch <shgtk.shruch@gmail.com>
2014-10-30 16:54:40 +0900

    Get slide image using async.

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 var https = require('https');
 var fs = require('fs');
+var async = require('async');
 
-function getImage (slideNum, cb) {
+var downloading = true;
+var i = 0;
+
+async.whilst(
+  function () {
+    return downloading;
+  },
+  function (callback) {
+    i++;
+    getImage(i, callback);
+  },
+  function () {
+    console.log('all slide downloaded.');
+    fs.unlinkSync(fixNum(i, 3) + '.jpg');
+  }
+);
+
+function getImage (slideNum, callback) {
   var url = 'https://s3-ap-northeast-1.amazonaws.com/i.schoo/images/class/slide/';
   var classNum = process.argv[2] + '/';
   var ext = '-1024.jpg';
-  var f = fs.createWriteStream(slideNum + '.jpg');
+  var f = fs.createWriteStream(fixNum(slideNum, 3) + '.jpg');
 
   https.get(url + classNum + slideNum + ext, function (res) {
     res.on('data', function (chunk) {
@@ -14,15 +32,18 @@ function getImage (slideNum, cb) {
 
     res.on('end', function () {
       f.end();
-      if (res.statusCode !== 200) cb(slideNum);
+      if (res.statusCode !== 200) {
+        downloading = false;
+      }
+      callback();
     });
   });
 }
 
-for (var i = 1; i < 20; i++) {
-  getImage(i, function (j) {
-    console.log('all slide downloaded.');
-    fs.unlinkSync(j + '.jpg');
-  });
+function fixNum (num, figures) {
+  var str = num.toString();
+  while (str.length < figures) {
+    str = '0' + str;
+  }
+  return str;
 }
-
